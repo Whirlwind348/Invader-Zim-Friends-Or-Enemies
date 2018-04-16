648c8c1773ef280791d72fe4b1867bf96c9dd128
Whirlwind348 <36898917+Whirlwind348@users.noreply.github.com>
2018-04-16 11:08:59 -0400

    Initial

---
+++ Assets/2D/Jump.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+	float Y;
+	Animator Sprite;
+	Rigidbody2D Physic;
+	bool inAir;
+	bool Enabled;
+	bool Falling;
+
+	// Use this for initialization
+	void Start () {
+		Enabled = true;		
+		Sprite = gameObject.GetComponent<Animator>();
+		Physic = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+
+	// Update is called once per frame
+	void Update(){
+		Y = Physic.velocity.y;
+		Sprite.SetFloat("Y Vel", Y);
+		if (Y < 0 && inAir == true && Falling == false) {
+			Falling = true;
+		}
+	}
+	void LateUpdate(){
+		if (Y == 0 && inAir == true && Falling == true) {
+			Sprite.SetTrigger ("Land");
+			inAir = false;
+			Falling = false;
+			Enabled = true;
+		}
+	}
+void FixedUpdate()
+		{
+		if(Input.GetKeyDown(KeyCode.Space) && Enabled == true){
+			Enabled = false;
+			Sprite.SetTrigger ("Jump");
+			Physic.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+			inAir = true;
+			}
+ 		
+
+		}
+		
+}
---
+++ Assets/2D/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	float X;
+	float invX;
+	float Dir;
+	Rigidbody2D Zim;
+	// Use this for initialization
+	void Start () {
+		Zim = gameObject.GetComponent<Rigidbody2D>();
+		X = Zim.velocity.x;
+		invX = -X;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		X = Zim.velocity.x;
+		invX = -X;
+		Dir = Input.GetAxis ("Horizontal");
+	}
+	void FixedUpdate(){
+		if (Input.GetButton ("Horizontal")) {
+			if (Dir > 0) {
+				
+			}
+			if (Dir < 0) {
+				Zim.AddForce (new Vector2 (0.1f, 0), ForceMode2D.Impulse);
+			}
+		}
+		if (Input.GetKeyUp ("Horizontal")) {
+			Zim.AddForce (new Vector2 (invX, 0), ForceMode2D.Impulse);
+		}
+	}
+}
